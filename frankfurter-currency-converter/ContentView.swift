1c1ac688eea2827375d854db59bcafaccd8c2444
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2025-04-16 02:02:13 +0300

    iOS: initial commit for project setup

---
+++ frankfurter-currency-converter/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  File name: ContentView.swift
+//  Project name: frankfurter-currency-converter
+//  Workspace name: Untitled 1
+//
+//  Created by: nothing-to-add on 16/04/2025
+//  Using Swift 6.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frankfurter-currency-converter/frankfurter_currency_converterApp.swift
@@ -0,0 +1,20 @@
+//
+//  File name: frankfurter_currency_converterApp.swift
+//  Project name: frankfurter-currency-converter
+//  Workspace name: Untitled 1
+//
+//  Created by: nothing-to-add on 16/04/2025
+//  Using Swift 6.0
+//  Copyright (c) 2023 nothing-to-add
+//
+
+import SwiftUI
+
+@main
+struct frankfurter_currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
