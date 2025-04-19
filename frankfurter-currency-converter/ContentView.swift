76bf47bb2304e075a397122b2522b475de7bbb65
nothing-to-add <36647739+nothing-to-add@users.noreply.github.com>
2025-04-19 23:30:45 +0300

    make UI more animated

--- frankfurter-currency-converter/ContentView.swift
+++ frankfurter-currency-converter/ContentView.swift
@@ -152,9 +152,13 @@ struct CurrencyListView: View {
             VStack(alignment: .trailing) {
                 Text(viewModel.getCurrencyValue(for: currency))
                     .font(.headline)
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: viewModel.getCurrencyValue(for: currency))
                 Text(String(format: "1 \(viewModel.selectedCurrency?.code ?? "") = %.2f", currency.rate))
                     .font(.subheadline)
                     .foregroundColor(.gray)
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: currency.rate)
             }
             .padding(0)
         }
@@ -164,6 +168,8 @@ struct CurrencyListView: View {
         .cornerRadius(10)
         .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 2)
         .contentShape(Rectangle())
+        .opacity(currency.code == viewModel.selectedCurrency?.code ?? "" ? 0 : 1)
+        .animation(.easeInOut, value: viewModel.selectedCurrency?.code)
         .onTapGesture {
             viewModel.selectCurrency(currency)
         }
@@ -173,14 +179,16 @@ struct CurrencyListView: View {
     private func selectedCurrencyCard() -> some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(viewModel.selectedCurrency?.code ?? "")
+                Text(viewModel.selectedCurrency?.code ?? "Code")
                     .font(.headline)
                     .foregroundStyle(.white)
-                if let currency = viewModel.selectedCurrency {
-                    Text(CurrencyDescription().getDescription(for: currency))
-                        .font(.subheadline)
-                        .foregroundColor(.white.opacity(0.7))
-                }
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: viewModel.selectedCurrency?.code)
+                Text(CurrencyDescription().getDescription(for: viewModel.selectedCurrency ?? Currency(code: "None", rate: 0.0)))
+                    .font(.subheadline)
+                    .foregroundColor(.white.opacity(0.7))
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: viewModel.selectedCurrency?.code)
             }
             
             Spacer()
